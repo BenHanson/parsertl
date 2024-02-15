07f6c49af4af521282f0f511efdd52b96258cde3
BenHanson <jamin.hanson@gmail.com>
2024-02-15 15:29:54 +0000

    Added parenthesis to avoid warnings

--- include/parsertl/rules.hpp
+++ include/parsertl/rules.hpp
@@ -61,7 +61,7 @@ namespace parsertl
             bool operator<(const symbol& rhs_) const
             {
                 return _type < rhs_._type ||
-                    _type == rhs_._type && _id < rhs_._id;
+                    (_type == rhs_._type && _id < rhs_._id);
             }
 
             bool operator==(const symbol& rhs_) const
@@ -94,7 +94,7 @@ namespace parsertl
                 bool operator<(const rhs& rhs_) const
                 {
                     return _symbols < rhs_._symbols ||
-                        _symbols == rhs_._symbols && _prec < rhs_._prec;
+                        (_symbols == rhs_._symbols && _prec < rhs_._prec);
                 }
             };
 
